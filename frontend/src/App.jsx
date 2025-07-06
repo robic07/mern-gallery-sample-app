f752b2756074c82d082d0c05b8f0bb70b0fc4bf2
robic08 <rborja.cheetah@gmail.com>
2025-07-07 02:54:53 +0800

    add s on todo to test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
 <section className="title-banner py-4 bg-success text-white" id="title-banner">
       <div className="container">
         <div className="row">
-          <h4>Todo Image Gallery</h4>
+          <h4>Todo Image Gallerys</h4>
         </div>
       </div>
     </section>
